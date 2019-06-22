d4c346768176faa84030d76645158a6c0a42ec93
wollf2008 <43559182+wollf2008@users.noreply.github.com>
2019-06-22 02:14:40 -0600

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pointposition : MonoBehaviour
+public class Move : MonoBehaviour
 {
-
-    public Transform WuTi;
 
     public float speed = 1;
 
@@ -18,29 +16,30 @@ public class pointposition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //上
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            transform.position += transform.forward * speed;
-        }
-        //下
-        else if (Input.GetKey(KeyCode.DownArrow))
+ 
+
+        if (Input.GetKey(KeyCode.A))
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
-            transform.position += transform.forward * speed;
+            GetComponent<Rigidbody2D>().velocity = transform.right * speed;
         }
-        //左
-        else if (Input.GetKey(KeyCode.LeftArrow))
+
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.eulerAngles = new Vector3(0, 270, 0);
-            transform.position += transform.forward * speed;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody2D>().velocity = transform.right * speed;
         }
-        //右
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKeyUp(KeyCode.A))
         {
-            transform.eulerAngles = new Vector3(0, 90, 0);
-            transform.position += transform.forward * speed;
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2 (0,0);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
     }
+
 }
